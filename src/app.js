57882a629231d859db8d9cad2383b2b5f0b184b7
Darren Rose <thorn086@gmail.com>
2020-01-31 22:47:28 -0600

    updated error handler

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-
+const errorHandler = require('./error-handler')
 const app = express()
 
 const morganOption = (NODE_ENV === 'production' 
@@ -19,15 +19,6 @@ app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  if(NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
-  } else {
-    console.error(error)
-    response = { message: error.message, error }
-  }
-  res.status(500).json(response)
-})
+app.use(errorHandler)
 
 module.exports = app
