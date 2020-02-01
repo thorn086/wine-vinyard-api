14de176cbf0f18200f71eb3370fd243ff8e76a04
Darren Rose <thorn086@gmail.com>
2020-01-31 22:51:48 -0600

    updated script and tests

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const app = require('./app')
 const { PORT } = require('./config')
 
+app.get('/api/*', (req,res)=>{
+  res.json({ok:true});
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
