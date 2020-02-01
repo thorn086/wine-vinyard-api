b2ad10605a3486663591e81f9616e8af9b3631e7
Darren Rose <thorn086@gmail.com>
2020-01-31 23:39:15 -0600

    added migrations and seeds

---
+++ seeds/seed.wines.sql
@@ -0,0 +1,33 @@
+INSERT INTO wines (wineCat,date, company_name,name,content,rating)
+VALUES
+     (   "wineCat": 'Red Wine',
+        "date": 2000,
+        "company_name": 'Horn Duck',
+        "name":'Marlot',
+        "content":' This is a dry wine with a hint of Oak flavor.',
+        "rating": 92
+    ),
+
+      (  "wineCat": 'White Wine',
+        "date": 2017,
+        "name":'Pino Noir',
+        "company_name": '1000 Buffalos',
+        "content":' This is a sweet wine to help remind you of the Fall Season.',
+        "rating": 90
+    ), 
+
+     (   "wineCat": 'Red Wine',
+        "date": 2000,
+        "name":'Malboc',
+        "company_name": 'Bare Foot',
+        "content":' This is a semi-sweet wine with all the taste of Summer.',
+        "rating": 94
+    ), 
+
+     (   "wineCat": 'White Wine',
+        "date": 2018,
+        "name":'Chardinay',
+        "company_name": 'Duck Bill',
+        "content":' This is a rather dry wine that works well with fish.',
+        "rating": 89
+    )
